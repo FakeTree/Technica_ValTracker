a5ba553b59be57d0af2c428843410283c858faf1
internette.explorer <internette.explorer@protonmail.ch>
2024-09-07 00:43:49 +1000

    update gitignore, update required modules to include gson

--- Technica_ValTracker/src/main/java/module-info.java
+++ Technica_ValTracker/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.example.technica_valtracker {
     requires javafx.fxml;
 
     requires org.kordamp.ikonli.javafx;
+    requires com.google.gson;
 
     opens com.example.technica_valtracker to javafx.fxml;
     exports com.example.technica_valtracker;

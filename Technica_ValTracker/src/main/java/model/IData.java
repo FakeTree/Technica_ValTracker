341de9b52ba379d904d8f1490f694bb96aa60728
internette.explorer <internette.explorer@protonmail.ch>
2024-09-09 00:41:52 +1000

    add generic database interface

---
+++ Technica_ValTracker/src/main/java/model/IData.java
@@ -0,0 +1,21 @@
+package model;
+
+/**
+ * Interface for the <> Data Access Objects that handles
+ * CRUD database operations for each respective DAO.
+ * The database is composed of several tables which are all linked
+ * by a single foreign key, 'userId'.
+ */
+public interface IData {
+    /**
+     * Add a new object to the database.
+     * @param object The object to add.
+      */
+    public void addNew(Object object);
+    /**
+     * Retrieves a database entry.
+     * @param userId The userId of the given object to retrieve.
+     * @returns The object with the given id.
+     */
+    public Object get(int userId);
+}

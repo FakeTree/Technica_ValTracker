c9cc2a2b07f5782984254f95d1e1220b133cfaef
Bozzzza <98837961+Bozzzza@users.noreply.github.com>
2024-09-08 17:13:54 +1000

    menu bar added but not functional
    Managed to get a window up with a menu bar at the top, but i dont think menubar and menuitems are clickable in javafx so going to look into a workaround

---
+++ Technica_ValTracker/src/main/java/com/example/technica_valtracker/MenuController.java
@@ -0,0 +1,27 @@
+package com.example.technica_valtracker;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.*;
+
+public class MenuController {
+
+    @FXML
+    private Menu TTeMenu;
+    @FXML
+    private Menu HomeMenu;
+    @FXML
+    private Menu TrainerMenu;
+    @FXML
+    private Menu FriendsMenu;
+    @FXML
+    private Menu AboutMenu;
+    @FXML
+    private Menu AccountMenu;
+
+
+
+
+
+
+}

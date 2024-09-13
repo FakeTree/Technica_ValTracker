7368e3b1d5c0a044deb1fdc1c1decc680d0d4db7
internette.explorer <internette.explorer@protonmail.ch>
2024-09-13 14:49:12 +1000

    del constants file

--- Technica_ValTracker/src/main/java/com/example/technica_valtracker/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.example.technica_valtracker;
-
-public class Constants {
-    public static String RIOT_API_KEY = "RGAPI-6a36cc3f-bd06-45c7-851b-97942c275df0";
-    public static String[] requestHeaders = new String[] { "X-Riot-Token", RIOT_API_KEY };
-}

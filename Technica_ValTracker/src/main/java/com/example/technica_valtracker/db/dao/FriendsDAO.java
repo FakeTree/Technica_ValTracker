69a8450c8bb55b89ac6f550352122c6611dd3043
codyhenshaw <n11272554@qut.edu.au>
2024-10-08 15:54:08 +1000

    commencing the friends functionality

---
+++ Technica_ValTracker/src/main/java/com/example/technica_valtracker/db/dao/FriendsDAO.java
@@ -0,0 +1,18 @@
+package com.example.technica_valtracker.db.dao;
+
+import com.example.technica_valtracker.db.DbConnection;
+import com.example.technica_valtracker.db.model.User;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+import java.sql.ResultSet;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+
+public class FriendsDAO {
+
+
+
+}

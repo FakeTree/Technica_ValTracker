eebda9b499d4a30d88f25b6ca16cba0db4e54253
codyhenshaw <n11272554@qut.edu.au>
2024-10-23 15:56:58 +1000

    added buttons to access manage friends page, updated look of manage friends page

--- Technica_ValTracker/src/main/java/com/example/technica_valtracker/controller/Manage_FriendsController.java
+++ Technica_ValTracker/src/main/java/com/example/technica_valtracker/controller/Manage_FriendsController.java
@@ -1,40 +1,114 @@
 package com.example.technica_valtracker.controller;
 
+import atlantafx.base.theme.Styles;
 import com.example.technica_valtracker.Constants;
 import com.example.technica_valtracker.HelloApplication;
 import com.example.technica_valtracker.UserManager;
 import com.example.technica_valtracker.api.ResponseBody;
+import com.example.technica_valtracker.api.error.ErrorMessage;
 import com.example.technica_valtracker.db.model.Champion;
 import com.example.technica_valtracker.db.model.League;
 import com.example.technica_valtracker.db.model.Summoner;
 import com.example.technica_valtracker.db.model.User;
-import com.example.technica_valtracker.utils.PasswordUtils;
 import com.example.technica_valtracker.utils.URLBuilder;
-import com.example.technica_valtracker.utils.Validation;
+import static com.example.technica_valtracker.api.Query.getQuery;
+import static com.example.technica_valtracker.utils.Deserialiser.*;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.concurrent.Task;
+import javafx.concurrent.WorkerStateEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.stage.Stage;
-import javafx.event.ActionEvent;
-import javafx.scene.Parent;
-import javafx.scene.Node;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
+import java.util.concurrent.*;
+
+import org.jetbrains.annotations.NotNull;
 
 public class Manage_FriendsController {
 
     @FXML
-    private TextField emailTextField;
+    private ListView<String> friendsListView; // A list of friends
     @FXML
-    private Button addFriendButton;
+    private TextField emailTextField; // A text field for entering a friend's email
     @FXML
-    private ListView<String> friendsListView;
+    private VBox friendContainer; // Container to hold friend details and actions
     @FXML
-    private Button deleteFriendButton;
+    private Button onAddConfirm; // Button to add a new friend
+    @FXML
+    private Button onDelete; // Button to delete a friend
 
 
+    @FXML
+    private void OnMatchHistoryMenu(ActionEvent actionEvent) throws IOException {
+
+        //switch to the match history scene
+        Stage stage = (Stage) friendsListView.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("match_history-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1024, 768);
+        stage.setScene(scene);
+    }
+
+    @FXML
+    private void OnManageFriendsMenuClick(ActionEvent actionEvent) throws IOException {
+        //switch to the Friends scene
+        Stage stage = (Stage) friendsListView.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Manage-Friends.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1024, 768);
+        stage.setScene(scene);
+
+    }
+    @FXML
+    private void OnFriendsMenuClick(ActionEvent actionEvent) throws IOException {
+        //switch to the Friends scene
+        Stage stage = (Stage) friendsListView.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Friends-View.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1024, 768);
+        stage.setScene(scene);
+
+    }
+    @FXML
+    private void onAddConfirm() {
+        // Action when the 'Add' button is pressed
+        String newFriend = emailTextField.getText();
+        if (!newFriend.isEmpty()) {
+            friendsListView.getItems().add(newFriend);
+            emailTextField.clear(); // Clear the text field after adding
+        }
+    }
+
+    @FXML
+    private void onDelete() {
+        // Action when the 'Delete' button is pressed
+        String selectedFriend = friendsListView.getSelectionModel().getSelectedItem();
+        if (selectedFriend != null) {
+            friendsListView.getItems().remove(selectedFriend);
+        }
+    }
+
+    @FXML
+    public void onLogOutMenuClick(ActionEvent actionEvent) throws IOException {
+        //switch to the login/signup screen
+        Stage stage = (Stage) friendsListView.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 450, 450);
+        stage.setScene(scene);
+    }
 
 
 }
